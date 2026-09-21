cdb5c5761dc240bf515330b1990e1d721b1d0524
Tiago Igor <taigoti@proton.me>
2026-09-20 22:12:28 -0300

    feat: adiciona constantes do projeto

---
+++ models/constantes.py
@@ -0,0 +1,17 @@
+CONHECIMENTOS_EXIGIDOS = {
+    "python",
+    "lógica",
+    "git",
+    "banco de dados",
+    "html"
+}
+
+TURNOS_DISPONIVEIS = ("manhã", "tarde")
+
+MOTIVOS_DESAPROVACAO = {
+    "idade": "Sua idade é menor que 16 anos!",
+    "equipe": "O projeto exige o trabalho em equipe!",
+    "conhecimentos": "Seus conhecimentos não são compatíveis com o projeto!",
+    "turno": "Seu turno não é compatível com o projeto!",
+    "pontuacao": "Sua pontuação total foi menor que 5 pontos!"
+}
