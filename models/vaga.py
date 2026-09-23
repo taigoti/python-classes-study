32613e2170c6f98fec7683f82483d81bcfcae4b0
Tiago Igor <taigoti@proton.me>
2026-09-23 00:15:21 -0300

    feat(vaga): calcula pontuação de acordo com pontuações definidas

--- models/vaga.py
+++ models/vaga.py
@@ -1,14 +1,15 @@
 from models.candidato import Candidato
 
 class Vaga:
-    def __init__(self, id: int, titulo: str, descricao: str, conhecimentos: set, turno: set):
+    def __init__(self, id: int, titulo: str, descricao: str, conhecimentos: set, turno: set, pontuacao: dict):
         self.id = id
         self.titulo = titulo
         self.descricao = descricao
         self.conhecimentos = conhecimentos
         self.turno = turno
+        self.pontuacao = pontuacao
 
-    def validar_candidato(self, candidato: Candidato) -> list[str]:
+    def validar_requisitos(self, candidato: Candidato) -> list[str]:
         motivos = []
 
         if candidato.idade < 16:
@@ -24,3 +25,21 @@ class Vaga:
             motivos.append("Turno não disponível")
         
         return motivos
+
+    def calcular_pontuacao(self, candidato: Candidato) -> int:
+        pontuacao = 0
+
+        if candidato.trabalho_em_equipe:
+            pontuacao += self.pontuacao['equipe']
+        
+        if candidato.turno in self.turno:
+            pontuacao += self.pontuacao['turno']
+        
+        pontuacao += len(
+            candidato.conhecimentos.intersection(self.conhecimentos)
+            ) * self.pontuacao['conhecimento']
+        
+        if candidato.computador_proprio:
+            pontuacao += self.pontuacao['computador_proprio']
+        
+        return pontuacao
