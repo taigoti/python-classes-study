2ae67e700661c82c6288ab7a33fe70a1873bf224
Tiago Igor <taigoti@proton.me>
2026-09-24 23:27:08 -0300

    feat(main): instancia objetos das classes

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 from models.candidato import Candidato
-from validators.candidato_validator import *
+from models.inscricao import Inscricao
+from models.vaga import Vaga
 
-candidato = Candidato.cadastrar()
 
-conhecimentos_compativeis, conhecimentos_faltantes = validar_conhecimentos(candidato)
+candidato = Candidato(
+    nome="João Silva",
+    idade=20,
+    curso="Engenharia de Software",
+    semestre=3,
+    email="joao.silva@me.com",
+    turno="tarde",
+    trabalho_em_equipe=True,
+    computador_proprio=True,
+    conhecimentos={"Python", "Java", "SQL"}
+  )
 
-motivos = validar_desclassificacao(
-    candidato.idade, candidato.trabalho_em_equipe,
-    conhecimentos_compativeis, conhecimentos_faltantes,
-    candidato.turno)
+vaga = Vaga(
+    id=1,
+    titulo="Desenvolvedor de Software",
+    descricao="Vaga para desenvolvedor de software com experiência em Python e Java.",
+    conhecimentos={"Python", "Java", "SQL", "JavaScript"},
+    turno={"manhã", "tarde"},
+    pontuacao={
+        "equipe": 2,
+        "turno": 1,
+        "conhecimento": 3,
+        "computador_proprio": 1
+    }
+  )
 
-print(candidato)
-
-if motivos:
-  print("CLASSIFICAÇÃO: NÃO APROVADO")
-  print("Motivo(s):")
-
-  for motivo in motivos:
-    print(f"-> {motivo}")
-
-else:
-  pontuacao = calcular_pontuacao(candidato, conhecimentos_compativeis)
-
-  print(f"Pontuação final: {pontuacao}")
-
-  print(validar_pontuacao(pontuacao))
+inscricao = Inscricao(candidato, vaga)
